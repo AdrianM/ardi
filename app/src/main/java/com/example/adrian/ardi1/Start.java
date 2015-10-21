2b12005dfcf8931671e58af651eb6af1efb7dd95
AdrianM <adrian.morgenegg@gmail.com>
2015-10-21 21:45:23 +0200

    clean up

--- app/src/main/java/com/example/adrian/ardi1/Start.java
+++ app/src/main/java/com/example/adrian/ardi1/Start.java
@@ -24,7 +24,7 @@ public class Start extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action AMO", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
